3cbbcb592977d4bdcf8ba36108e276de7e4472fa
Bill Miller <bmiller@postmedia.com>
2022-04-08 11:47:21 -0400

    installer

--- PythonXPC/setup.py
+++ PythonXPC/setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='StreetPerfect',
-	version='11.0.0',
+	version='11.0.1',
 	packages=['StreetPerfect'],
 	url='http://www.streetperfect.com/',
 	author='Bill Miller',
