74ed30a7ea655d01c9a15ab6cdd41a0c29dd76e3
Bill Miller <bmiller@postmedia.com>
2022-10-05 13:52:05 -0400

    sp batch demo fix

--- sp_batch.py
+++ sp_batch.py
@@ -1,29 +1,26 @@
-import logging, datetime, time, os, argparse
+import logging, datetime, time, os, argparse, json
 from PythonModule.StreetPerfect.HttpClient import HttpClient
 from PythonModule.StreetPerfect import StreetPerfectException
 from PythonModule.StreetPerfect.Models import *
 
+_ScriptVer = "v1.2"
+
 """
 # bmiller, aug 2022
-# the sp_creds.py file (not in repo) must contain one or more StreetPerfect creds in a dictionary
+# the sp_creds.json file (not in repo) must contain one or more StreetPerfect creds in a dictionary
 # currently only the aws and dev sites have batch (dev is limited to 200 recs)
 # you can override this file by passing the creds to the script
 
-creds = {
-	'dev' : {
-		'api_id' : 'user@postmedia.com',
-		'api_key' : 'api-key',
-		'api_url' : 'https://apidev.streetperfect.com/api/',
+{
+	"dev" : {
+		"api_id" : "user@postmedia.com",
+		"api_key" : "api-key",
+		"api_url" : "https://apidev.streetperfect.com/api/"
 		},
-	'aws' : {
-		'api_id' : 'user@postmedia.com',
-		'api_key' : 'api-key',
-		'api_url' : 'https://stage.streetperfect-dev.com/api/',
-		},
-	'prod' : {
-		'api_id' : 'user@postmedia.com',
-		'api_key' : 'api-key',
-		'api_url' : 'https://api.streetperfect.com/api/',
+	"prod" : {
+		"api_id" : "user@postmedia.com",
+		"api_key" : "api-key",
+		"api_url" : "https://api.streetperfect.com/api/"
 		},
 }
 
@@ -32,7 +29,7 @@ creds = {
 logger = None
 
 def sp_batch(sp_api_id, sp_api_key, sp_url, cfg, input_file, out_good_recs, out_bad_recs, verify_ssl=True):
-    """
+    '''
     Runs a list of addresses through a remote StreetPerfect batch processor
     :param sp_api_id: str, StreetPerfect user id (your email addr)
     :param sp_api_key: str, your api key (generated on SP site)
@@ -42,21 +39,21 @@ def sp_batch(sp_api_id, sp_api_key, sp_url, cfg, input_file, out_good_recs, out_
     :param out_good_recs: str, the output CSV file containing validated/corrected addresses
     :param out_bad_recs: str, the output CSV file containing errors
     :return: nothing
-    """
+    '''
     try:
 
-        logger.info("Running StreetPerfect Batch Address Verifier")
+        logger.info('Running StreetPerfect Batch Address Verifier')
 
         client = HttpClient(sp_api_id, sp_api_key, url=sp_url, verify=verify_ssl)
 
         info = client.Info()
-        logger.info("\n".join(f"{k}={v}" for k, v in info.info.items()))
+        logger.info('\n'.join(f'{k}={v}' for k, v in info.info.items()))
 
         # check status first
         r = client.caBatchStatus()
         if r.status == 'Running':
             client.caBatchStopTask();
-            logger.info("batch task shows running, so stopping task")
+            logger.info('batch task shows running, so stopping task')
             while 1:
                 # now wait till its done
                 # possible statuses are: Unknown, Error, Empty, Starting, Running, Stopping, Stopped, InputReady, OutputReady
@@ -67,13 +64,13 @@ def sp_batch(sp_api_id, sp_api_key, sp_url, cfg, input_file, out_good_recs, out_
                 time.sleep(5)
 
         # must delete any exiting input & output
-        logger.info("deleting all IO files")
-        client.caBatchClean("all")
+        logger.info('deleting all IO files')
+        client.caBatchClean('all')
         time.sleep(5)
         # upload as a multipart-form
-        logger.info("uploading input file; %s", input_file)
+        logger.info('uploading input file; %s', input_file)
         r = client.caBatchUploadForm(input_file, isZip=input_file.lower().endswith('.zip'))
-        logger.info(f"caBatchUploadForm response: {r.msg}")
+        logger.info(f'caBatchUploadForm response: {r.msg}')
  
         # fetch status
         r = client.caBatchStatus();
@@ -81,37 +78,40 @@ def sp_batch(sp_api_id, sp_api_key, sp_url, cfg, input_file, out_good_recs, out_
 
         # batch status must be 'InputReady' to run
         # we're using the default CSV config and pass the column ordinals
-        if r.status == "InputReady":
+        if r.status == 'InputReady':
 
             # start the batch task!
-            logger.info("starting batch task")
+            logger.info('starting batch task')
             client.caBatchStartTask(cfg)
             
-            #todo, make this more robust so it doesn't poll forever
             while 1:
                 # now wait till its done
                 # possible statuses are: Unknown, Error, Empty, Starting, Running, Stopping, Stopped, InputReady, OutputReady
+
                 r = client.caBatchStatus()
                 print(f'BatchRun Status: {r.status}')
-                if r.status == 'OutputReady':
+                if r.status not in ['Starting', 'Running']:
                     logger.info(r.runInfo)
                     break
                 time.sleep(5)
 
-            client.caBatchDownloadTo('log', "StreetPerfectBatch.log")
-            client.caBatchDownloadTo('zip', "StreetPerfectBatch.zip")
-            client.caBatchDownloadTo('output', out_good_recs)
-            client.caBatchDownloadTo('output_errors', out_bad_recs)
+            if r.status == 'OutputReady':
+                client.caBatchDownloadTo('log', 'StreetPerfectBatch.log')
+                client.caBatchDownloadTo('zip', 'StreetPerfectBatch.zip')
+                client.caBatchDownloadTo('output', out_good_recs)
+                client.caBatchDownloadTo('output_errors', out_bad_recs)
                     
-            logger.info(client.caBatchDownload('log').replace('\r', ''))
+                logger.info(client.caBatchDownload('log').replace('\r', ''))
+            else:
+               logger.error(f"wrong batch status '{r.status}', should be 'OutputReady'")
         else:
             logger.error(f"wrong batch status '{r.status}', should be 'InputReady'")
             
 
     except StreetPerfectException as e:
-        logger.critical(f"StreetPerfectHttpException: {e}")
+        logger.critical(f'StreetPerfectHttpException: {e}')
     except Exception as e:
-        logger.critical(f"Exception: {e}")
+        logger.critical(f'Exception: {e}')
     finally:
         try:
             r = client.caBatchStatus()
@@ -125,11 +125,11 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(prog='sp_batch', 
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-                                     description='Executes a StreetPerfect batch validation process against a passed list of addresses.')
+                                     description=f'{_ScriptVer}, Executes a StreetPerfect batch validation process against a passed list of addresses.')
     parser.add_argument('-spApiId', help='your StreetPerfect client id (email addr)')
     parser.add_argument('-spApiKey', help='your StreetPerfect api key')
     parser.add_argument('-spApiUrl', help='alt StreetPerfect site api url (other than api[dev].streetperfect.com)')
-    parser.add_argument('-spCredKey', default='aws', help='if using the sp_creds.py file, specify the creds key')
+    parser.add_argument('-spCredKey', help='if using the sp_creds.json file, specify the creds key')
     parser.add_argument('-noSSLval', action='store_true', help='disable ssl validation')
 
     parser.add_argument('-logDir', help='the path to the logging directory (optional)')
@@ -149,9 +149,9 @@ if __name__ == '__main__':
 
     # configure logging to file
     if args.logDir:
-        filenameString = args.logDir + '/sp_batch_' + datetime.datetime.now().strftime("%Y-%m-%d") + '.log'
+        filenameString = args.logDir + '/sp_batch_' + datetime.datetime.now().strftime('%Y-%m-%d') + '.log'
     else:
-        filenameString = './sp_batch_' + datetime.datetime.now().strftime("%Y-%m-%d") + '.log'
+        filenameString = './sp_batch_' + datetime.datetime.now().strftime('%Y-%m-%d') + '.log'
     logging.basicConfig(level=logging.INFO,
                         format='[%(asctime)-15s] %(levelname)-8s [%(name)-12s] %(message)s',
                         handlers=[logging.FileHandler(filenameString), logging.StreamHandler()] )
@@ -174,17 +174,18 @@ if __name__ == '__main__':
         sp_url = args.spApiUrl
     else:
         try:
-            import sp_creds
-            creds = sp_creds.creds[args.spCredKey]
+            with open('sp_creds.json') as f:
+                sp_creds = json.load(f)            
+            creds = sp_creds[args.spCredKey]
             sp_api_id = creds['api_id']
             sp_api_key = creds['api_key']
             sp_url = creds['api_url']
-            print('read sp_creds.py')
+            print('read sp_creds.json')
         except Exception as e:
-            print(f'sp_creds.py read error: {e}')
+            print(f'sp_creds.json read error: {e}')
 
     if not sp_api_id and not sp_api_key:
-        raise Exception("no StreetPerfect credentials found (require api id and key)")
+        raise Exception('no StreetPerfect credentials found (require api id and key)')
 
     verify_ssl = True
     if args.noSSLval:
