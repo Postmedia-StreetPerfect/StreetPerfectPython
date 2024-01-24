ed154e11e8febfab07341454d49f264990300095
QuickWire <bmiller@postmedia.com>
2024-01-24 16:56:00 -0500

    ver

--- PythonModule/setup.py
+++ PythonModule/setup.py
@@ -2,12 +2,12 @@
 from setuptools import setup, find_packages
 
 setup(name='StreetPerfect',
-	version='12.0.5',
+	version='12.1.0',
 	packages=['StreetPerfect'],
 	url='http://www.streetperfect.com/',
 	author='Bill Miller',
 	author_email='bmiller@postmedia.com',
-	license='Copyright © 1993-2023, Postmedia Network Inc',
+	license='Copyright © 1993-2024, Postmedia Network Inc',
 	description='This package interfaces with the StreetPerfect low level (XPC) API.',
 	#long_description=open('README.txt').read(),
 	include_package_data=True,
