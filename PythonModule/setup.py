f349550b5efab235edf82fecf869c9ac79c5666a
Bill Miller <bmiller@postmedia.com>
2025-10-16 10:31:13 -0400

    12.3.3

--- PythonModule/setup.py
+++ PythonModule/setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='StreetPerfect',
-	version='12.3.2',
+	version='12.3.3',
 	packages=['StreetPerfect'],
 	url='http://www.streetperfect.com/',
 	author='Bill Miller',
